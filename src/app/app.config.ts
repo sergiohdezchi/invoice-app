5ef105d97ae64e0940d193f8fc1994a52150db20
sergio <sergiohdez.chi@gmail.com>
2025-06-19 10:37:00 -0600

    Routing and invoice page configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,20 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { MAT_DATE_LOCALE, MatNativeDateModule } from '@angular/material/core';
 
 import { routes } from './app.routes';
+import { authInterceptor } from './services/http-interceptor.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    ),
+    importProvidersFrom(MatNativeDateModule),
+    { provide: MAT_DATE_LOCALE, useValue: 'es-MX' }
+  ]
 };
---
+++ src/app/pages/invoice-page/invoice-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-invoice-page',
+  imports: [],
+  templateUrl: './invoice-page.component.html',
+  styleUrl: './invoice-page.component.scss'
+})
+export class InvoicePageComponent {
+
+}
